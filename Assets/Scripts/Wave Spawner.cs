19367f5a2e909145312072d11284e78aa5b664f0
Nermey <elik19898@gmail.com>
2023-08-03 16:44:58 +0300

    fix spawn enemies after defeat

--- Assets/Scripts/Wave Spawner.cs
+++ Assets/Scripts/Wave Spawner.cs
@@ -7,6 +7,7 @@ public class WaveSpawner : MonoBehaviour
     public Transform enemyPrefab;
     public Transform spawnPoint;
 
+
     public float timeBetweenWaves = 10f;
     private float countdown = 2f; // время до начала первой волны (позже время между волнами (счетчик))
     private int waveIndex = 0;
@@ -24,15 +25,21 @@ public class WaveSpawner : MonoBehaviour
         countdown -= Time.deltaTime; // счетчик уменьшается каждые 2f (2с) на 1
 
         countdown = Mathf.Clamp(countdown, 0f, Mathf.Infinity);
-        if (Mathf.Round(countdown) <= 10f)
-        {
-            waveCountdownText.text = "Next wave: " + string.Format("{0:00.00}", countdown);
+        if (!GameManager.gameIsOver)
+        { 
+            if (Mathf.Round(countdown) <= 10f)
+            {
+                waveCountdownText.text = "Next wave: " + string.Format("{0:00.00}", countdown);
+            }
+            else
+            {
+                waveCountdownText.text = "";
+            }
         }
         else
         {
-            waveCountdownText.text = "";
+            waveCountdownText.text = "";     
         }
-        
     }
 
     IEnumerator SpawnWave () // IENumerator позволяет поставить код на паузу при выполнении
@@ -40,6 +47,10 @@ public class WaveSpawner : MonoBehaviour
         waveIndex++;
         for (int i = 0; i < waveIndex; i++)
         {
+            if (GameManager.gameIsOver)
+            {
+                break;
+            }
             SpawnEnemy();
             yield return new WaitForSeconds(1.5f); // задержка по времени при выполнении кода
         }
